54065f304d6b153b27e4a2e4d372c0baa4f223f6
codersimone <97359905+codersimone@users.noreply.github.com>
2022-05-12 03:32:15 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+function showMessage() {
+    let a = prompt ("Введите первое число");
+    let b = prompt ("Введите второе число");
+}
+
+function sumAddition() {
+    showMessage();
+    alert(Number(a) + Number(b));
+}
+
+
+
+
+
+function sumSubtraction() {
+    showMessage();
+    alert(Number(a) - Number(b));
+}
+
+function sumMultiplication() {
+    showMessage();
+    alert(Number(a) * Number(b));
+}
+
+function sumDivision() {
+    showMessage();
+    alert(Number(a) / Number(b));
+}
+
